9b1dcf36ec6944f2c6de05d89e2484c21fb51368
Pablo Alonso <jackercito@gmail.com>
2022-09-13 13:38:49 +0200

    commit

--- app.js
+++ app.js
@@ -46,12 +46,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/experimento', jwtCheck, experimento);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
 app.use(function (err, _req, _res, next) {
     let err2 = new Error('Not Found: ' + err);
     console.log("Err -> " + err2);
